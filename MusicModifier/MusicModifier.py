2b8464614286b689dea5a226f04ac38b1da09d93
Sanjay Ganeshan <sangan@live.com>
2015-08-20 16:07:34 -0400

    Will search for Artist-less and Title-less songs and ask to change them

--- MusicModifier/MusicModifier.py
+++ MusicModifier/MusicModifier.py
@@ -132,12 +132,7 @@ def getArtistAndAlbum(filename):
                 return parts[0],parts[1]
     return "artist","song" 
 
-def populateKnownSongsFromDirectory():
-    pass
-
-def searchDirectory():
-    path = raw_input("Path to Search?: ")
-    origPath = path
+def populateMP3sFromDirectory(path):
     numMP3s = 0
     allMp3s = []
     for path, dirs, files in os.walk(path):
@@ -148,18 +143,7 @@ def searchDirectory():
             numMP3s += 1
             fullFileName = os.path.join(path, filename)
             try:
-                mp3File = id3(fullFileName)
-                #print mp3File
-                #print "found and read file...",fullFileName
                 allMp3s.append(fullFileName)
-                if mp3File is None:
-                    print "No tags"
-                if "artist" in mp3File:
-                    currArtist = str(mp3File["artist"])[3:-2]
-                    if currArtist in knownArtists:
-                        knownArtists[currArtist] += 1
-                    else:
-                        knownArtists[currArtist] = 1
             except IOError,e:
                 pass                       
             except Exception,e:
@@ -167,12 +151,161 @@ def searchDirectory():
             else:
                 pass
     print "Found",numMP3s,"mp3 files"
+    return allMp3s
 
+def repeatChar(c,num):
+    s = ""
+    for i in range(num):
+        s += c
+    return s 
+
+def populateKnownArtistsFromDirectory(path):
+    numMP3s = 0
+    allMp3s = populateMP3sFromDirectory(path)
+    numRead = 0
+    stringToPrint = ""
+    global knownArtists
+    for fullFileName in allMp3s:
+        #print fullFileName
+        numMP3s += 1
+        try:
+            mp3File = id3(fullFileName)
+            if mp3File is None:
+                print "No tags"
+            if "artist" in mp3File:
+                currArtist = str(mp3File["artist"])[3:-2]
+                if currArtist in knownArtists:
+                    knownArtists[currArtist] += 1
+                else:
+                    knownArtists[currArtist] = 1
+            numRead += 1
+            if numRead % 10 == 0:
+                backspaces = repeatChar('\b',len(stringToPrint) + 1)
+                stringToPrint = "Done processing " + str(numRead) + " files"
+                print backspaces + stringToPrint,
+                #print stringToPrint
+                #raw_input()
+        except IOError,e:
+            pass                       
+        except Exception,e:
+            print "Exception:",e
+        else:
+            pass
+    print ""
+    writeKnownArtistsToFile()
+
+def writeKnownArtistsToFile(artistFile = "knownArtists.txt"):
+    global knownArtists
+    f = open(artistFile,"w")
+    for eachArtist in sorted(knownArtists.keys()):
+        f.write(eachArtist + " | " + str(knownArtists[eachArtist]))
+        f.write("\n")
+
+def populateKnownArtistsFromFile(artistFile = "knownArtists.txt"):
+    global knownArtists
+    try:
+        f = open(artistFile,"r")
+    except Exception,e:
+        return False
+    else:
+        knownArtists = {}
+        for line in f:
+            info = line.replace("\n","").split("|")
+            knownArtists[info[0]] = int(info[1])
+        return True
+    
+
+def populateKnownArtists():
+    alreadyExists = populateKnownArtistsFromFile()
+    if alreadyExists:
+        shouldRefresh = raw_input("I found an Artist File that already exists. Would you like me to update it? (Y/N)").upper() == "Y"
+        if shouldRefresh:
+            newPath = raw_input("Okay. What is the directory with your music?\n   --> ")
+            populateKnownArtistsFromDirectory(newPath)
+            print "Artist data refreshed"
+    else:
+        print "I could not find cached artist data. I have to recreate it."
+        newPath = raw_input("What is the directory with your music?\n   --> ")
+        populateKnownArtistsFromDirectory(newPath)
+        print "Artist data refreshed"
+
+def getMp3sWithoutArtist(allMp3s):
+    numRead = 0
+    stringToPrint = ""
+    mp3sWithoutArtists = []
+    numFound = 0
+    numMP3s = 0
+    global knownArtists
+    for fullFileName in allMp3s:
+        #print fullFileName
+        numMP3s += 1
+        try:
+            mp3File = id3(fullFileName)
+            if mp3File is None or not("artist" in mp3File):
+                mp3sWithoutArtists.append(fullFileName)
+                numFound += 1
+            numRead += 1
+            if numRead % 10 == 0:
+                backspaces = repeatChar('\b',len(stringToPrint) + 1)
+                stringToPrint = "Done processing " + str(numRead) + " files. Found " + str(numFound) + " files without an artist."
+                print backspaces + stringToPrint,
+                #print stringToPrint
+                #raw_input()
+        except IOError,e:
+            pass                       
+        except Exception,e:
+            print "Exception:",e
+        else:
+            pass
+    print ""
+    return mp3sWithoutArtists
+
+def getMp3sWithoutTitles(allMp3s):
+    numRead = 0
+    stringToPrint = ""
+    mp3sWithoutTitles = []
+    numFound = 0
+    numMP3s = 0
+    global knownArtists
+    for fullFileName in allMp3s:
+        #print fullFileName
+        numMP3s += 1
+        try:
+            mp3File = id3(fullFileName)
+            if mp3File is None or not("title" in mp3File):
+                mp3sWithoutTitles.append(fullFileName)
+                numFound += 1
+            numRead += 1
+            if numRead % 10 == 0:
+                backspaces = repeatChar('\b',len(stringToPrint) + 1)
+                stringToPrint = "Done processing " + str(numRead) + " files. Found " + str(numFound) + " files without an artist."
+                print backspaces + stringToPrint,
+        except IOError,e:
+            pass                       
+        except Exception,e:
+            print "Exception:",e
+        else:
+            pass
+    print ""
+    return mp3sWithoutTitles
+        
+def writeMp3(filename,artist,title):
+    mp3File = id3(filename)
+    mp3File["title"] = title
+    mp3File["artist"] = artist
+    mp3File.save()
+
+def searchDirectory():
+    populateKnownArtists()
+    path = raw_input("Path to Search and Tag?: ")
+    origPath = path
+    allMp3s = populateMP3sFromDirectory(path)
     numRight = 0
     numTotal = 0
     shouldBreak = False
     path = origPath
 
+    '''
     #Print Found artists
     print "Found Artists:"
     artistArr = []
@@ -182,31 +315,53 @@ def searchDirectory():
     artistArr.sort(key=operator.itemgetter(1),reverse=True)
     for eachItem in artistArr:
         print eachItem
-    
-    res = getArtistAndAlbum("C:\Users\sanjay\Music\Miku\[60fps Full]  Senbonzakura _One Thousand Cherry Trees_- Hatsune Miku  DIVA English Romaji.mp3")
-    print "Guessing...Artist:",res[0],"  Song Title:",res[1]
-    for filename in allMp3s:
-        if not filename.lower().endswith('.mp3'):
-            continue
-        fullFileName = os.path.join(path, filename)
+    '''
+    mp3sWithoutArtists = getMp3sWithoutArtist(allMp3s)
+    for fullFileName in mp3sWithoutArtists:
         try:
             res = getArtistAndAlbum(fullFileName)
             if res[0] != 'artist' and res[1] != 'song':
                 print "File:",fullFileName
-                print "Guessing...Artist:",res[0],"  Song Title:",res[1]
+                artist = res[0]
+                title = res[1]
+                print "Guessing...Artist:",artist,"  Song Title:",title
                 ans = raw_input("Right? (Y/N/Exit)  -->  ")
                 if ans == "Exit":
                     shouldBreak = True
                     break
                 elif ans.upper() == "Y":
                     numRight += 1
+                    writeMp3(fullFileName,artist,title)
                 numTotal += 1
 
         except Exception,e:
-            raise e
+            print e
         else:
             pass
+    del mp3sWithoutArtists
+    mp3sWithoutTitles = getMp3sWithoutTitles(allMp3s)
+    for fullFileName in mp3sWithoutTitles:
+        try:
+            res = getArtistAndAlbum(fullFileName)
+            if res[0] != 'artist' and res[1] != 'song':
+                print "File:",fullFileName
+                artist = res[0]
+                title = res[1]
+                print "Guessing...Artist:",artist,"  Song Title:",title
 
+                ans = raw_input("Right? (Y/N/Exit)  -->  ")
+                if ans == "Exit":
+                    shouldBreak = True
+                    break
+                elif ans.upper() == "Y":
+                    numRight += 1
+                    writeMp3(fullFileName,artist,title)
+                numTotal += 1
+
+        except Exception,e:
+            print e
+        else:
+            pass
     if numTotal == 0:
         numTotal = 1
     print numRight,"Guessed Correctly.",numTotal,"In Total.",int(float(numRight)/numTotal * 100),"% correct."
